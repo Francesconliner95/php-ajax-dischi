cb2064deb1bc36642e40882d214a87441d350ee3
Francesconliner <francescodivincenzo95@outlook.it>
2020-12-17 17:01:34 +0100

    Esercizio

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
-<?php include 'dischi.php' ?>
+<?php
+ include 'dischi_php.php';
+?>
 
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
@@ -6,20 +8,25 @@
         <meta charset="utf-8">
         <title></title>
         <link rel="stylesheet" href="dist/app.css">
-        <script src="http://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.7.6/handlebars.min.js">
         </script>
     </head>
     <body>
         <header>
-            <input type="radio" name="call" value="php">
-            <label for="php">PHP</label>
-            <input type="radio" name="call" value="ajax">
-            <label for="ajax">AJAX</label>
+            <h1>
+                Brani caricati tramite file PHP
+            </h1>
+            <div class="choise">
+                <input type="radio" name="call" value="php">
+                <label for="php">PHP</label>
+                <input type="radio" name="call" value="ajax">
+                <label for="ajax">AJAX</label>
+            </div>
         </header>
         <main>
             <div class="container">
-                <div class="cards-container">
+                <div class="cards-container" id="php">
                 <?php  foreach ($dischi as $key => $value) {?>
                     <div class="card">
 
@@ -36,19 +43,21 @@
                 <?php
                 }
                 ?>
-                <!-- <script id="card" type="text/x-handlebars-template">
-                    <div class="card">
-                        <div class="card-top">
-                            <img src="{{context[0].poster}}" alt="">
+                </div>
+                <div class="cards-container" id="ajax">
+                    <script id="card" type="text/x-handlebars-template">
+                        <div class="card">
+                            <div class="card-top">
+                                <img src="{{poster}}" alt="">
+                            </div>
+                            <div class="card-bottom">
+                                <p>{{title}}</p>
+                                <p>{{author}}</p>
+                                <p>{{genre}}</p>
+                                <p>{{year}}</p>
+                            </div>
                         </div>
-                        <div class="card-bottom">
-                            <p>{{context[0].title}}</p>
-                            <p>{{context[0].author}}</p>
-                            <p>{{context[0].genre}}</p>
-                            <p>{{context[0].year}}</p>
-                        </div>
-                    </div>
-                </script> -->
+                    </script>
                 </div>
             </div>
         </main>
--- main.js
+++ main.js
@@ -1,27 +1,59 @@
 $(document).ready(function(){
 
-    // const source = $("#card").html();
-    // const template = Handlebars.compile(source);
+    var is_php = true;
+
+    var radios = $('input:radio[name=call]');
+    if(radios.is(':checked') === false) {
+        radios.filter('[value=php]').prop('checked', true);
+    };
+
+    $('#ajax').hide();
+    $('#php').show();
+
+    $(radios).on('change', function(){
+        if(radios.val()=='php'){
+            radios.val('ajax');
+            is_php = false;
+            $('#php').hide();
+            $('#ajax').show();
+            $('h1').text('Brani caricati tramite chiamata AJAX');
+        }
+        else{
+            radios.val('php');
+            is_php = true;
+            $('#ajax').hide();
+            $('#php').show();
+            $('h1').text('Brani caricati tramite file PHP');
+        }
+        console.log(radios.val());
+        console.log(is_php);
+    });
+
+    const source = $("#card").html();
+    const template = Handlebars.compile(source);
 
     $.ajax({
-        url: 'dischi.php',
+        url: 'dischi_ajax.php',
         method: 'GET',
         success: function(data){
-            console.log('data: ' + data);
-            // for (var i = 0; i < data.length; i++) {
-            //     var context = {
-            //         poster: data[i].poster,
-            //         title: data[i].title,
-            //         author: data[i].author,
-            //         year: data[i].year,
-            //     };
-            //     var html = template(context);
-            //     $('.card-container').append(html);
-            // }
+            console.log(data);
+            for (var i = 0; i < data.length; i++) {
+                var context = {
+                    poster: data[i].poster,
+                    title: data[i].title,
+                    genre: data[i].genre,
+                    author: data[i].author,
+                    year: data[i].year,
+                };
+                var html = template(context);
+                $('#ajax').append(html);
+            }
         },
         error: function(){
             console.log('errore');
         }
     });
-    
+
+
+
 });
